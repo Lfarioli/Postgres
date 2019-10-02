2116832d80a67144cd7810edffee7f6039d49f55
Lorenzo Farioli <52935800+Lfarioli@users.noreply.github.com>
2019-10-02 09:54:18 +0200

    exlude system schema

--- PerPostgres/take_schema.sh
+++ PerPostgres/take_schema.sh
@@ -1,22 +1,31 @@
 #!bin/bash
 
 DATABASE_LIST=$(psql -tl | grep -v "^  " |awk '{print $1}' |grep -v "^$" | egrep -vw "template0|template1" )
-P=0
+
 
 for DB in $DATABASE_LIST; do
 
-echo " Database  $DB" 
 
-SCHEMA=$(psql -t $DB -c "SELECT schema_name FROM information_schema.schemata;")
+	SCHEMA=$(psql -t $DB -c "SELECT schema_name FROM information_schema.schemata;")
 
+	echo " Database  $DB" 			
 
+		
 		for SH in $SCHEMA; do
 
-		SIZE=$(psql -t -c "SELECT SUM(pg_total_relation_size(quote_ident(schemaname) || '.' || quote_ident(tablename)))::BIGINT FROM pg_tables WHERE schemaname = '$SH';")	
+			if [ "$SH" != "pg_catalog" ] && [ "$SH" != "pg_toast_temp_1" ] &&  [ "$SH" != "pg_temp_1" ] &&  [ "$SH" != "pg_toast" ] &&  [ "$SH" != "information_schema" ]; then
 
-		echo "$SH -- $SIZE"
+			
 
+			SIZE=$(psql -t -c "SELECT SUM(pg_total_relation_size(quote_ident(schemaname) || '.' || quote_ident(tablename)))::BIGINT FROM pg_tables WHERE schemaname = '$SH';")	
+
+			echo " schema $SH size $SIZE"
+
+
+			fi
+		
 		done	
 
 
 done
+
