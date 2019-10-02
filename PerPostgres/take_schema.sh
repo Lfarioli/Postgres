bc721019cfa6c0d6f227358197478f4a8d51dff5
Lorenzo Farioli <52935800+Lfarioli@users.noreply.github.com>
2019-10-02 14:36:24 +0200

    set to null the emmpty variable

--- PerPostgres/take_schema.sh
+++ PerPostgres/take_schema.sh
@@ -9,18 +9,27 @@ for DB in $DATABASE_LIST; do
 
 	SCHEMA=$(psql -t $DB -c "SELECT schema_name FROM information_schema.schemata;")
 
-	echo " Database  $DB" 	
-
 		
 		for SH in $SCHEMA; do
 
 			if [ "$SH" != "pg_catalog" ] && [ "$SH" != "pg_toast_temp_1" ] &&  [ "$SH" != "pg_temp_1" ] &&  [ "$SH" != "pg_toast" ] &&  [ "$SH" != "information_schema" ]; then
 
-			
 
-			SIZE=$(psql -t $DB -c "SELECT SUM(pg_total_relation_size(quote_ident(schemaname) || '.' || quote_ident(tablename)))::BIGINT FROM pg_tables WHERE schemaname ='$SH';")	
+			SIZE=$(psql -t $DB -c "SELECT SUM(pg_total_relation_size(quote_ident(schemaname) || '.' || quote_ident(tablename)))::BIGINT FROM pg_tables WHERE schemaname ='$SH';" | head -n 1)	
 
 			NUMTABLE=$(psql -t $DB -c "select count(*) from information_schema.tables where table_schema ='$SH';")
+
+			
+			if  [ "$SIZE" == '    ' ]; then
+
+				SIZE='null'
+			fi
+
+
+			if  [ $NUMTABLE -eq 0 ]; then
+
+				NUMTABLE='null'
+			fi
 
 			echo "$DB $SH $SIZE $NUMTABLE " >> report.txt
 
