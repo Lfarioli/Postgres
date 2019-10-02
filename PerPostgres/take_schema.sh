d638d904a68cc3da9c88d8f2faf769e4b9445371
Lorenzo Farioli <52935800+Lfarioli@users.noreply.github.com>
2019-10-02 10:23:40 +0200

    add table size in schema

--- PerPostgres/take_schema.sh
+++ PerPostgres/take_schema.sh
@@ -17,9 +17,11 @@ for DB in $DATABASE_LIST; do
 
 			
 
-			SIZE=$(psql -t -c "SELECT SUM(pg_total_relation_size(quote_ident(schemaname) || '.' || quote_ident(tablename)))::BIGINT FROM pg_tables WHERE schemaname = '$SH';")	
+			SIZE=$(psql -t $DB -c "SELECT SUM(pg_total_relation_size(quote_ident(schemaname) || '.' || quote_ident(tablename)))::BIGINT FROM pg_tables WHERE schemaname ='$SH';")	
 
-			echo " schema $SH size $SIZE"
+			NUMTABLE=$(psql -t $DB -c "select count(*) from information_schema.tables where table_schema ='$SH';")
+
+			echo " schema: $SH size: $SIZE table: $NUMTABLE "
 
 
 			fi
